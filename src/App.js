870fa32dc585f49d19fba411bd0560f710641746
RGHuynh <randall.g.huynh@gmail.com>
2018-11-28 15:58:02 -0500

    Change navigation file name to index

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import navigation from './components/navigation'
 import './App.css';
 
 class App extends Component {
--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+
 class Navigation extends Component{
   render(){
     return(
