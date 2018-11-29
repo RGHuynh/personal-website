f2eb9ab205542c73d323b1844513c16ea324ef1f
RGHuynh <randall.g.huynh@gmail.com>
2018-11-29 10:16:34 -0500

    impementing commit hamburger stack icon

--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
+import HamburgerStack from '../icons/hamburgerStack'
 import './index.css';
-
 
 class Navigation extends Component{
   render(){
     return(
       <nav className="navbar navbar__color navbar--size">
         <div className="hamburgerStack">
-
+          <HamburgerStack />
         </div>
       </nav>
     );
