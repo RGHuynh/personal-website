3945df6ff8c6d11a5c38ca6700a2a8c54ba55bbd
RGHuynh <randall.g.huynh@gmail.com>
2018-12-02 14:55:16 -0500

    Add: experience component

---
+++ src/components/Experience/index.jsx
@@ -0,0 +1,12 @@
+import Reach from 'react';
+import './index.css';
+
+export default function Experience() {
+    return(
+        <section>
+            <div>Reactjs</div>
+            <div>NodeJS</div>
+            <div>PostgreSQL</div>
+        </section>
+    );
+}
