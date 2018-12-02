a1161000e218a3747b187d9934964a68da5633eb
RGHuynh <randall.g.huynh@gmail.com>
2018-12-02 15:02:41 -0500

    Add: placeholder elements

--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -4,7 +4,11 @@ import './index.css';
 export default function Project() {
     return(
         <section>
-            project
+            <h1>Project</h1>
+            <div>image</div>
+            <h2>Random Restaurant</h2>
+            <p>some stuff</p>
+            <a>button</a>
         </section>
     );
 }
