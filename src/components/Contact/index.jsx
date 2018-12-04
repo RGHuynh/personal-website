d7039c3be2c2ad8e71a942dfb2892dc9a53190aa
RGHuynh <randall.g.huynh@gmail.com>
2018-12-04 11:50:22 -0500

    Add class to section element

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -3,26 +3,28 @@ import './index.css';
 
 export default function Contact() {
     return(
-        <section>
-            <form>
-                <div className="form-group">
-                    <label>Name</label>
-                    <input className="form-control" type="text" value="name" />
-                </div>
+        <section className="contact-container">
+            <div className="container">
+                <form>
+                    <div className="form-group">
+                        <label>Name</label>
+                        <input className="form-control" type="text" value="name" />
+                    </div>
 
-                <div className="form-group">
-                    <label>Email</label>
-                    <input className="form-control" type="text" value="Enter Email" />
-                </div>
+                    <div className="form-group">
+                        <label>Email</label>
+                        <input className="form-control" type="text" value="Enter Email" />
+                    </div>
 
-                
-                <div className="form-group">
-                    <label>Message</label>
-                    <textarea className="form-control" type="text" row="5" value="Your Message"></textarea>
-                </div>
-                
-                <input type="buttone" />
-            </form>
+                    
+                    <div className="form-group">
+                        <label>Message</label>
+                        <textarea className="form-control" type="text" row="5" value="Your Message"></textarea>
+                    </div>
+                    
+                    <input type="buttone" />
+                </form>
+            </div>
         </section>
     );
 }
