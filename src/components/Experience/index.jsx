d9af0ef5ae87f0665730a0af61bf3aef2c80cf55
RGHuynh <randall.g.huynh@gmail.com>
2018-12-03 22:31:37 -0500

    style: experience section

--- src/components/Experience/index.jsx
+++ src/components/Experience/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import ReactJsIcon from '../../images/reactjs.svg';
 import './index.css';
 
 export default function Experience() {
     return(
         <section>
-            <div>Reactjs</div>
+            
+            <div className="exp-wrap">
+                <img className="exp-icon" src={ReactJsIcon}></img>
+                <h1 className="exp-title">Reactjs</h1>
+            </div>
+            
             <div>NodeJS</div>
             <div>PostgreSQL</div>
         </section>
