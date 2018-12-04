216b4e63877cd146bee3114a784a0fa247718f1b
RGHuynh <randall.g.huynh@gmail.com>
2018-12-03 19:23:52 -0500

    Add header background

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import Navigation from '../Navigation';
-import ProfilePic from '../../images/profile.jpeg'
+import ProfilePic from '../../images/profile.jpeg';
+import Background1 from '../../images/background1.svg'
 import './index.css';
 
 export default function Header() {
     return(
         <header >
             <Navigation />
-            <img className="icon" src={ProfilePic}></img>
-            <div className="header-description">
-                <h1 className="header-description-name">Randall Huynh</h1>
-                <h2 className="header-description-title">Full Stack Developer</h2>
+            <div className="container">
+                <div className="portfolio" style={{backgroundImage: `url(${Background1})`}}>
+                    <img className="icon" src={ProfilePic}></img>
+                        <div className="header-description">
+                            <h1 className="header-description-name">Randall Huynh</h1>
+                            <h2 className="header-description-title">Full Stack Developer</h2>
+                        </div>
+                </div>
             </div>
-
         </header>
     )
 }
