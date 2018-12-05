c3b9abf75597c0cef1751169305fa3d7846b862a
RGHuynh <randall.g.huynh@gmail.com>
2018-12-04 19:14:22 -0500

    Add scrollreveal to project

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import Experience from './components/Experience';
 import Project from './components/Project';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import ScrollReveal from 'scrollreveal';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+    ScrollReveal().reveal('.mastHead')
+  }
   render() {
     return (
         <div className="App">
