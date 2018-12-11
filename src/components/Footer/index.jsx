66e96a22f04dbf8e6483bedc9ff81011f1fc9b35
RGHuynh <randall.g.huynh@gmail.com>
2018-12-11 10:51:04 -0500

    Add linkedin svg

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import GithubImg from '../../images/github.svg';
+import LinkedinImg from '../../images/linkedin.svg';
 import './index.css';
 
 export default function Footer() {
     return(
         <footer className="footer-container">
-            <img className="footer-img" src={GithubImg}></img>
-            <a href="http://github.com">Github</a>
+            <a className="footer-img-container" href="https://github.com/RGHuynh">
+                <img className="footer-img" src={GithubImg}></img>
+            </a>
+            <a className="footer-img-container" href="https://www.linkedin.com/in/randallhuynh/">
+                <img className="footer-img" src={LinkedinImg}></img>
+            </a>
         </footer>
     );
 }
