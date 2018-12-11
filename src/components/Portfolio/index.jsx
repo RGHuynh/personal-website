a990342b9e521f363787f6e43f4d5a338e00fa3f
RGHuynh <randall.g.huynh@gmail.com>
2018-12-11 13:52:05 -0500

    Add element to component

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import BackgroundImage2 from '../../images/background2.svg';
 import './index.css';
 
-export default function Project() {
+export default function Portfolio() {
     return(
-        <section className="project-container" style={{backgroundImage: `url(${BackgroundImage2})`}}>
-            <h1 className="project-title__color title">PROJECTS</h1>
+        <section className="portfolio-container" style={{backgroundImage: `url(${BackgroundImage2})`}}>
+            <h1 className="portfolio-title__color title">PROJECTS</h1>
             <div>image</div>
             <h2>Random Restaurant</h2>
             <p>some stuff</p>
