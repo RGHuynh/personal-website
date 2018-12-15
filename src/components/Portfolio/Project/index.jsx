d33752094bfff1fefe07b52bc67fdcfaca4411a7
RGHuynh <randall.g.huynh@gmail.com>
2018-12-15 12:41:13 -0500

    wip project section screen size

--- src/components/Portfolio/Project/index.jsx
+++ src/components/Portfolio/Project/index.jsx
@@ -5,12 +5,14 @@ export default function Project({title, image, subTitle, body, backgroundStyle, 
     return(
         <div className="project-container" style={ backgroundStyle }>
             <h1 className="portfolio-subTitle portfolio-title__color title" style={titleColor} >{title}</h1>
-            <img className="project-image" src={ image } />
-            <h2 className="portfolio-subTitle" style={ titleColor } >{ subTitle }</h2>
-            <p className="project-body" style={titleColor} >{ body }</p>
-            <a href={ urlLink } className="btn btn__position btn__size btn__color">
-                View
-            </a>
+            <div className="project-wrapper">
+                <img className="project-image" src={ image } />
+                <h2 className="portfolio-subTitle" style={ titleColor } >{ subTitle }</h2>
+                <p className="project-body" style={titleColor} >{ body }</p>
+                <a href={ urlLink } className="btn btn__position btn__size btn__color">
+                    View
+                </a>
+            </div>
         </div>
     );
 }
